db5123eddba665da1a1f0b95bad5371e8de1d893
Arnoldo Tejeda <arnoldotejeda10@gmail.com>
2020-06-08 10:27:32 -0500

    Version predeterminada

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//Favor de agregar su nombre para indicar que se tiene control del
+// las versiones del repositorio:D
+// *
+// *
+// * Tejeda Arnoldo
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("***** Proyecto Asteroide *****");
+    }
+}
--- src/com/asteroid/Main.java
+++ src/com/asteroid/Main.java
@@ -3,6 +3,7 @@ package com.asteroid;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hola mundo");
+	    // write your code here
     }
 }
